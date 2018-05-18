fecd729ab87e3ddfaa340b707790e0875f160158
hanksxu2017 <335334851@qq.com>
2018-05-18 20:50:07 +0800

    任务推进

--- jeeweb-mybatis/src/main/java/cn/jeeweb/modules/sys/mapper/CourseStudentRecordMapper.java
+++ jeeweb-mybatis/src/main/java/cn/jeeweb/modules/sys/mapper/CourseStudentRecordMapper.java
@@ -2,8 +2,12 @@ package cn.jeeweb.modules.sys.mapper;
 
 import cn.jeeweb.modules.sys.entity.CourseStudentRecord;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface CourseStudentRecordMapper extends BaseMapper<CourseStudentRecord> {
 
-    void updateStatus(CourseStudentRecord.CourseStudentRecordStatus status);
+    void updateStatus(@Param("status")String status, @Param("studentIdList")List<String> studentIdList,
+                      @Param("courseRecId") String courseRecId);
 }
